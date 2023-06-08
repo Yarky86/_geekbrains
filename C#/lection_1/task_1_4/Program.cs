5583d334a4a06f98244a0e2e33cc842be82305ef
Yarky86 <yarky2006@mail.ru>
2023-06-09 00:56:13 +0300

    Create tssk_1_4 q q
    '

---
+++ C#/lection_1/task_1_4/Program.cs
@@ -0,0 +1,5 @@
+﻿double number3 = new Random().Next(1, 10); //from 1 to 9
+Console.WriteLine(number3);
+double number4 = new Random().Next(1, 10); //from 1 to 9
+Console.WriteLine(number4);
+Console.Write(number3 / number4);
