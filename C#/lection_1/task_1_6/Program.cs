d07ec49a8256b94a103321677748c38d6f636d4d
Yarky86 <yarky2006@mail.ru>
2023-06-10 00:37:46 +0300

    Create task_1_6

---
+++ C#/lection_1/task_1_6/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Enter five different numbers:");
+Console.WriteLine("a = 1");
+int a = 1;
+Console.WriteLine("b = 4");
+int b = 4;
+Console.WriteLine("c = 5");
+int c = 5;
+Console.WriteLine("d = 3");
+int d = 3;
+Console.WriteLine("e = 2");
+int e = 2;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Max = ");
+Console.WriteLine(max);
