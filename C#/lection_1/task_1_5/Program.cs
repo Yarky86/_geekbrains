e06b64520af4d99485044d80de9d89264495af27
Yarky86 <yarky2006@mail.ru>
2023-06-09 20:48:28 +0300

    Create task_1_5

---
+++ C#/lection_1/task_1_5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter Username: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "yarky")
+{
+    Console.Write("Hello, my dear friend, ");
+    Console.Write(username);
+    Console.WriteLine(", I have been wating for you for ages, really!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+}
