790fd04507eb11a3979bc3c21e7dcd03cba3e8be
Yarky86 <yarky2006@mail.ru>
2023-06-11 23:26:45 +0300

    Modify task_1_6

--- C#/lection_1/task_1_6/Program.cs
+++ C#/lection_1/task_1_6/Program.cs
@@ -1,14 +1,15 @@
 ﻿Console.WriteLine("Enter five different numbers:");
-Console.WriteLine("a = 1");
-int a = 1;
-Console.WriteLine("b = 4");
-int b = 4;
-Console.WriteLine("c = 5");
-int c = 5;
-Console.WriteLine("d = 3");
-int d = 3;
-Console.WriteLine("e = 2");
-int e = 2;
+
+Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.Write("d = ");
+int d = Convert.ToInt32(Console.ReadLine());
+Console.Write("e = ");
+int e = Convert.ToInt32(Console.ReadLine());
 
 int max = a;
 
