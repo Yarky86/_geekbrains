9240e6e76ed344f6011f35906b96f2fbada8ca74
Yarky86 <yarky2006@mail.ru>
2023-06-12 00:41:40 +0300

    Create seminar_1

---
+++ C#/seminars/seminar_1/Program.cs
@@ -0,0 +1,51 @@
+﻿// First Task 
+// Console.Clear();
+// Console.Write("Enter a natural number wich square you would like to get —> ");
+
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"square of {a} = {a*a}" );
+
+
+// Second task
+// Console.Clear();
+// Console.Write("Enter a first natural number —> ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter a second natural number, which you want to check if it is a square of the first number —> ");
+// int bb = Convert.ToInt32(Console.ReadLine());
+
+// if (b * b == bb)
+// {
+//     Console.WriteLine($"Oh, year, the number {bb} is a square of the {b}!");
+// }
+// else
+// {
+//     Console.WriteLine($"Oh, sorry, but the number {bb} is NOT a square of the {b}!");
+// }
+
+// Third task
+// Console.Clear();
+// Console.Write("Enter a natural number, from which you would like to get a sequence of all whole numbers from that number to its negative version —> ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int count = -c;
+
+// Console.Write($"There is the follow sequence of whole numbers from {-c} to {c} -> ");
+
+// while (count <= c)
+// {
+//     Console.Write($"{count} ");
+//     count++;
+// }
+
+
+// //Fourth task
+// Console.Clear();
+// Console.Write("Enter a natural number, from 100 to 999 —> ");
+// int d = Convert.ToInt32(Console.ReadLine());
+
+// while (d < 100 || d > 999)
+// {
+//     Console.Write("Please, enter a natural number, from 100 to 999 —> ");
+//     d = Convert.ToInt32(Console.ReadLine());
+// }
+// Console.Write($"The last number in the number {d} is {d % 10}");
